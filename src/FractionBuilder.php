ebec9b2f278ff6b91eb1481af6d08d757fe0413d
AJ Meireles <alvaro.meireles@live.com>
2025-05-29 14:47:42 -0300

    wip

--- src/FractionBuilder.php
+++ src/FractionBuilder.php
@@ -7,15 +7,14 @@ namespace Fraction;
 use Closure;
 use Fraction\Facades\Fraction;
 use Fraction\Jobs\FractionJob;
-use Illuminate\Contracts\Container\BindingResolutionException;
+use Fraction\Support\DependencyResolver;
 use Illuminate\Foundation\Application;
-use InvalidArgumentException;
 use Laravel\SerializableClosure\SerializableClosure;
-use ReflectionException;
-use ReflectionFunction;
 use RuntimeException;
 
-class FractionBuilder
+use function Illuminate\Support\defer;
+
+final class FractionBuilder
 {
     public array $before = [];
 
@@ -33,6 +32,7 @@ class FractionBuilder
         // ...
     }
 
+    /** @throws @throws ReflectionException|BindingResolutionException|InvalidArgumentException */
     public function __invoke(...$arguments): mixed
     {
         foreach ($this->before as $before) {
@@ -41,62 +41,28 @@ class FractionBuilder
             $before();
         }
 
-        $result = null;
-
-        /** @throws ReflectionException|BindingResolutionException|InvalidArgumentException */
-        $call = function () use ($arguments): mixed {
-            $reflection = new ReflectionFunction($this->closure);
-
-            $parameters = $reflection->getParameters();
-            $resolved   = [];
-
-            foreach ($parameters as $index => $parameter) {
-                if (array_key_exists($index, $arguments)) {
-                    $resolved[] = $arguments[$index];
-
-                    continue;
-                }
-
-                foreach ($parameter->getAttributes() as $attribute) {
-                    if (! str_contains($attribute->getName(), 'Illuminate\Container\Attributes')) {
-                        continue;
-                    }
-
-                    $instance = $attribute->newInstance();
-
-                    if (! method_exists($instance, 'resolve')) {
-                        continue;
-                    }
-
-                    $resolved[] = $instance->resolve($instance, $this->application);
-                }
-
-                $type = $parameter->getType();
-
-                if ($type && ! $type->isBuiltin()) {
-                    $resolved[] = $this->application->make($type->getName());
-                } elseif ($parameter->isDefaultValueAvailable()) {
-                    $resolved[] = $parameter->getDefaultValue();
-                } else {
-                    throw new InvalidArgumentException("Cannot resolve parameter \${$parameter->getName()} in [{$this->action}]");
-                }
-            }
-
-            return call_user_func_array($this->closure, $resolved);
-        };
-
         if ($this->queued) {
             dispatch(new FractionJob($this->action, $arguments, new SerializableClosure($this->closure)));
-        } elseif ($this->deferred) {
-            \Illuminate\Support\defer($call);
-        } else {
-            $result = $call();
 
-            foreach ($this->after as $after) {
-                $after = Fraction::get($after);
+            return true;
+        }
 
-                $after();
-            }
+        $call = $this->application->make(DependencyResolver::class, [
+            'action' => $this->action,
+        ]);
+
+        if ($this->deferred) {
+            defer(fn () => $call->resolve($this->closure, $arguments));
+
+            return true;
+        }
+
+        $result = $call->resolve($this->closure, $arguments);
+
+        foreach ($this->after as $after) {
+            $after = Fraction::get($after);
+
+            $after();
         }
 
         return $result;
--- src/Support/DependencyResolver.php
+++ src/Support/DependencyResolver.php
@@ -4,8 +4,67 @@ declare(strict_types=1);
 
 namespace Fraction\Support;
 
+use Closure;
+use Illuminate\Contracts\Container\BindingResolutionException;
+use Illuminate\Foundation\Application;
+use InvalidArgumentException;
+use ReflectionException;
+use ReflectionFunction;
+
 /** @internal */
-final class DependencyResolver
+final readonly class DependencyResolver
 {
-    // ...
+    public function __construct(
+        private string $action,
+        private Application $application,
+    ) {
+        //
+    }
+
+    /**
+     * //
+     *
+     * @throws ReflectionException|BindingResolutionException|InvalidArgumentException
+     */
+    public function resolve(Closure $closure, array $arguments = []): mixed
+    {
+        $reflection = new ReflectionFunction($closure);
+
+        $parameters = $reflection->getParameters();
+        $resolved   = [];
+
+        foreach ($parameters as $index => $parameter) {
+            if (array_key_exists($index, $arguments)) {
+                $resolved[] = $arguments[$index];
+
+                continue;
+            }
+
+            foreach ($parameter->getAttributes() as $attribute) {
+                if (! str_contains($attribute->getName(), 'Illuminate\Container\Attributes')) {
+                    continue;
+                }
+
+                $instance = $attribute->newInstance();
+
+                if (! method_exists($instance, 'resolve')) {
+                    continue;
+                }
+
+                $resolved[] = $instance->resolve($instance, $this->application);
+            }
+
+            $type = $parameter->getType();
+
+            if ($type && ! $type->isBuiltin()) {
+                $resolved[] = $this->application->make($type->getName());
+            } elseif ($parameter->isDefaultValueAvailable()) {
+                $resolved[] = $parameter->getDefaultValue();
+            } else {
+                throw new InvalidArgumentException("Cannot resolve parameter \${$parameter->getName()} in [{$this->action}]");
+            }
+        }
+
+        return call_user_func_array($closure, $resolved);
+    }
 }
