76c6799062e75102c2af6f8124fa7f23a5c276e5
AJ Meireles <alvaro.meireles@live.com>
2025-05-29 15:30:59 -0300

    renaming functions

--- src/helpers.php
+++ src/helpers.php
@@ -5,15 +5,15 @@ declare(strict_types=1);
 use Fraction\Facades\Fraction;
 use Fraction\FractionBuilder;
 
-if (! function_exists('fraction')) {
-    function fraction(string $action, Closure $closure): FractionBuilder
+if (! function_exists('execute')) {
+    function execute(string $action, Closure $closure): FractionBuilder
     {
         return Fraction::register($action, $closure);
     }
 }
 
-if (! function_exists('execute')) {
-    function execute(string $action, ...$args): mixed
+if (! function_exists('run')) {
+    function run(string $action, ...$args): mixed
     {
         /** @var FractionBuilder $builder */
         $builder = Fraction::get($action);
