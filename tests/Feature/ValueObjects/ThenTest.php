b24c9203faa8530edd7db405dfb261fbb1406d84
AJ Meireles <alvaro.meireles@live.com>
2025-05-30 13:36:13 -0300

    wip

--- tests/Feature/ValueObjects/ThenTest.php
+++ tests/Feature/ValueObjects/ThenTest.php
@@ -3,15 +3,16 @@
 declare(strict_types=1);
 
 use Fraction\Exceptions\PreventLoop;
+use Fraction\ValueObjects\Then;
 
 test('can instantiate', function () {
-    $then = new Fraction\ValueObjects\Then('foo', 'foo');
+    $then = new Then('foo', 'foo');
 
     expect($then->then)->toBe('foo');
 });
 
 test('cannot instantiate due same name', function () {
-    $then = new Fraction\ValueObjects\Then('__fraction.foo', 'foo');
+    $then = new Then('__fraction.foo', 'foo');
 
     expect($then->then)->toBe('foo');
 })->throws(PreventLoop::class, 'The hook "then" cannot be used to invoke itself.');
