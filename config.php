ede7e61e7d24fc3fb969f7309950392fbe5dfea3
AJ Meireles <alvaro.meireles@live.com>
2025-05-30 14:14:03 -0300

    Added default action path to the config file

--- config.php
+++ config.php
@@ -1,5 +1,15 @@
 <?php
 
 return [
+    /*
+    |--------------------------------------------------------------------------
+    | Action Path
+    |--------------------------------------------------------------------------
+    |
+    | This option defines the default path where the action files will be
+    | stored. You can change this path to suit your application's structure.
+    |
+    */
+
     'path' => base_path('app/Actions'),
 ];
