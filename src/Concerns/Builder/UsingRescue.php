19022f94977d2cac6b6e060074d8f7c44ce2a640
AJ Meireles <alvaro.meireles@live.com>
2025-05-31 13:49:14 -0300

    isolating methods in concerns

---
+++ src/Concerns/Builder/UsingRescue.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Fraction\Concerns\Builder;
+
+use Fraction\Configurable\RescuedUsing;
+
+trait UsingRescue
+{
+    /**
+     * Indicates if the action should be rescued.
+     */
+    private ?RescuedUsing $rescued = null;
+
+    /**
+     * Enable the action to be rescued.
+     *
+     * @return $this
+     */
+    public function rescued(mixed $default = null): self
+    {
+        $this->rescued = new RescuedUsing($default);
+
+        return $this;
+    }
+}
---
+++ src/Concerns/Builder/UsingThen.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Fraction\Concerns\Builder;
+
+use Fraction\ValueObjects\Then;
+use UnitEnum;
+
+trait UsingThen
+{
+    /**
+     * The array of "then" hooks.
+     *
+     * @var array<int, string>
+     */
+    private array $then = [];
+
+    /**
+     * Register a "then" hook.
+     */
+    public function then(string|UnitEnum $action): self
+    {
+        $this->then[] = new Then($this->action, $action);
+
+        return $this;
+    }
+}
